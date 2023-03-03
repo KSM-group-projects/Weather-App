bdad1d9b4acd999722e83c6ea782923aaaa09d85
Mohammed-Saif0 <mohammedsaif9513@gmail.com>
2023-03-03 13:04:50 +0530

    Added show more weather button and show less weather button

--- index.js
+++ index.js
@@ -2,7 +2,40 @@ import utils from './utils/utils.js';
 
 const searchButton = document.querySelector('#query-button');
 const searchInput = document.querySelector('#query-input');
-const weatherCard = document.querySelector('.weather-card');
+const weatherCard = document.querySelector('.todays-weather');
+const showMoreButton = document.querySelector("#more-weather-btn");
+const showLessButton = document.querySelector("#show-less-btn");
+const weatherSection = document.querySelector(".weather-section");
+
+const displayTodaysWeather = (data)=>{
+    weatherCard.innerText = "Address = "+data.resolvedAddress+" " +utils.fahrenheitToCelsius(data.days[0].tempmax).toFixed(2) +" Degree Celcius";
+    localStorage.setItem('location',data.address.slice(0,-1));
+    showMoreButton.style.display = "block"
+}
+
+const displayMoreWeathers =(data) =>{
+    let weatherSection = document.querySelector(".weather-section");
+    console.log(data)
+    for(let i=0;i<5;i++){
+        let eachWeather = document.createElement("div");
+        eachWeather.innerHTML = "date= "+data.days[0].datetime+" Address = "+data.resolvedAddress+" " +utils.fahrenheitToCelsius(data.days[i].tempmax).toFixed(2) +" Degree Celcius";
+        weatherSection.append(eachWeather)
+    }
+    showMoreButton.style.display = "none";
+    showLessButton.style.display = "block";
+}
+
+showLessButton.addEventListener('click' ,(e)=>{
+    let arr = weatherSection.children
+    Array.from(arr).forEach(x=>{
+        if(x.tagName == 'DIV' && x.id !== 'todays-weather')
+            weatherSection.removeChild(x)
+    })
+    showLessButton.style.display = "none";
+    showMoreButton.style.display = "block";
+})
+
+utils.queryLocation(searchButton,searchInput,displayTodaysWeather);
+utils.queryLocation(showMoreButton,"",displayMoreWeathers)
 
 
-utils.queryLocation(searchButton,searchInput,weatherCard);
--- utils/utils.js
+++ utils/utils.js
@@ -3,29 +3,39 @@ const fahrenheitToCelsius = (temp) =>{
     return (temp-32) * (5/9);
 }
 
-const fetchWeather = async(inputElement,divElement) =>{
-    const query = inputElement.value;
-    inputElement.value = '';
+const fetchWeather = async(inputElement,callback) =>{
+    let query = ''
+    if(typeof(inputElement) == "object")
+        query = inputElement.value;
+    else{
+        query = localStorage.getItem('location')
+    }
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}%7D?unitGroup=us&key=HEG4F8F8KWRVJJMYWBWT267MR&contentType=json`;
     try{
+        console.log("here")
         let response = await fetch(url);
         let data = await response.json();
-        divElement.innerText = "Address = "+data.resolvedAddress+" " +fahrenheitToCelsius(data.days[0].tempmax).toFixed(2) +" Degree Celcius";
+        callback(data)
     }
     catch(error){
-        divElement.innerText =  `Location ${query} not found`;
+        console.log(error)
+        alert(`${query} location not found`)
     }
 }
 
+
+
 //Function to get display (on divElement) weather whenever the buttonElement is clicked 
-const queryLocation =(buttonElement,inputElement,divElement) =>{
+const queryLocation =(buttonElement,inputElement,callback) =>{
     buttonElement.addEventListener('click',function(){
-        fetchWeather(inputElement,divElement)   
+        fetchWeather(inputElement,callback)   
     })
 }
 
+
 let obj = {
-    queryLocation :queryLocation
+    queryLocation :queryLocation,
+    fahrenheitToCelsius:fahrenheitToCelsius,
 }
 
 export default obj;
